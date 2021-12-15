b16ff5043e39d91ee786330abe151ff005e875c0
1768894221 <1768894221@qq.com>
2021-12-15 22:13:30 +0800

    EFCoreCreate

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_EF_Enviroment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
